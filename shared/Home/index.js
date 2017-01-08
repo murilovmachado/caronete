7666aaf9ce00f65036978513ee9036cccc7561c6
Murilo Machado <mvmacha@thoughtworks.com>
2017-01-08 16:20:11 -0300

    iOS and Android sharing same code

---
+++ shared/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+import styles from './styles';
+
+const Home = props =>
+  <View style={styles.container}>
+    <Text style={styles.welcome}>Welcome to Caronete</Text>
+  </View>;
+
+export default Home;
